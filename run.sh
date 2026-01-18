85de7c9cee123624d0febb69682009e7136bc8c1
Tomas Reveron <84748259+YoshiHD4K@users.noreply.github.com>
2026-01-18 23:57:55 +0000

    muchos mas archivos

--- run.sh
+++ run.sh
@@ -8,6 +8,9 @@ echo "🚀 Iniciando servidor de Hytale..."
 # Asegúrate de que la ruta a Assets.zip sea correcta. 
 # Si el archivo está en la raíz, usa ../Assets.zip desde la carpeta Server
 java -Xms$MEM_START -Xmx$MEM_MAX \
+     --enable-native-access=ALL-UNNAMED \
      -XX:+UseG1GC \
      -jar Server/HytaleServer.jar \
      --assets ./Assets.zip
+     --ip 0.0.0.0 \
+     --port 5520
